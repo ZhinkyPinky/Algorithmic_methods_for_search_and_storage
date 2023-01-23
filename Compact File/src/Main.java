c762a2e75b7ab6487e10a22b39ddbc27000d42d6
ZhinkyPinky <jonathan.engstrom@gmail.com>
2023-01-23 16:47:34 +0100

    stuff

--- Compact File/src/Main.java
+++ Compact File/src/Main.java
@@ -1,35 +1,26 @@
-import java.awt.*;
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.IntBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        doStuff(new File("Compact File/abba-1"), 1, 7);
-        doStuff(new File("Compact File/abba-2"), 2, 7);
-        doStuff(new File("Compact File/tenbits-2"), 2, 10);
+        compact(new File("Compact File/abba-1"), 1, 7);
+        compact(new File("Compact File/abba-2"), 2, 7);
+        compact(new File("Compact File/tenbits-2"), 2, 10);
     }
 
-    private static void doStuff(File file, int B, int bo) throws IOException {
-        try (FileInputStream fileInputStream = new FileInputStream(file);
+    private static void compact(File file, int B, int bo) throws IOException {
+        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
              BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("Compact File/" + file.getName() + "-output"))
         ) {
-            byte[] bytes = fileInputStream.readAllBytes();
-            int[] ints = new int[(int) (bytes.length / B)];
-            for (int i = 0; i < ints.length; i++) {
-                for (int j = 0; j < B; j++) {
-                    ints[i] = (ints[i] << (8 * (B - 1 - j))) | (bytes[B * i + j] & 0xFF) << (8 * (B - 1 - j));
-                }
-            }
-
             int buffer = 0;
             int bitsInBuffer = 0;
-            for (int i : ints) {
+            for (int i = 0; i < (file.length() / B); i++) {
+                int integer = 0;
+                for (int j = 0; j < B; j++) {
+                    integer = (integer << (8 * (B - 1 - j))) | (bufferedInputStream.read() & 0xFF) << (8 * (B - 1 - j));
+                }
+
                 bitsInBuffer += bo;
-                buffer = (buffer << bo) | i;
+                buffer = (buffer << bo) | integer;
 
                 while (bitsInBuffer >= 8) {
                     bitsInBuffer -= 8;
--- Word Frequencies/src/Main.java
+++ Word Frequencies/src/Main.java
@@ -5,22 +5,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new BufferedReader(new FileReader("Word Frequencies/test.txt")));
-        scanner.useDelimiter("\\W+");
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("Word Frequencies/test.txt")))) {
+            scanner.useDelimiter("\\W+");
 
-        HashMap<String, Integer> frequencies = new HashMap<>();
-        while (scanner.hasNext()){
-            String s = scanner.next().toLowerCase(Locale.ROOT);
+            HashMap<String, Integer> frequencies = new HashMap<>();
+            while (scanner.hasNext()) {
+                String s = scanner.next().toLowerCase(Locale.ROOT);
 
-            if (frequencies.containsKey(s)){
-                frequencies.put(s, frequencies.get(s) + 1);
-            } else {
-                frequencies.put(s, 1);
+                if (frequencies.containsKey(s)) {
+                    frequencies.put(s, frequencies.get(s) + 1);
+                } else {
+                    frequencies.put(s, 1);
+                }
+
+                frequencies.forEach((key, value) -> System.out.println(key + ": " + value));
             }
         }
-
-        frequencies.forEach((key, value) -> System.out.println(key + ": " + value));
-
-        scanner.close();
     }
 }
