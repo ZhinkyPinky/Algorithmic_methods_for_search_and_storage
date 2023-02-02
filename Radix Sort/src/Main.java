1c37d4109f0960077eddecfd436dbebd44e0a595
ZhinkyPinky <jonathan.engstrom@gmail.com>
2023-02-02 16:57:20 +0100

    Word Frequencies V2 start

--- Radix Sort/src/Main.java
+++ Radix Sort/src/Main.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         long timeBefore = System.currentTimeMillis();
-        //sortTestString(new File("Radix Sort/bible-lines.txt"));
+        sortTestString(new File("Radix Sort/bible-lines.txt"));
         //sortTestString(new File("Radix Sort/test.txt"));
-        sortTestString(new File("Radix Sort/testString.txt"));
+        //sortTestString(new File("Radix Sort/strangestrings.txt"));
         //sortTestInt(new File("Radix Sort/ints.txt"));
+        //sortTestInt(new File("Radix Sort/someones.txt"));
         //sortTestInt(new File("Radix Sort/test.txt"));
         long timeAfter = System.currentTimeMillis();
         System.out.println((timeAfter - timeBefore) / 1000.0);
@@ -19,10 +20,10 @@ public class Main {
             ArrayList<Integer> aTemp = new ArrayList<>();
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                aTemp.add(Integer.valueOf(line));
+                aTemp.add((int) Long.parseLong(line));
             }
 
-            Integer[] a = MSD.sort(aTemp.toArray(new Integer[0]), new Integer[aTemp.size()], (-1 >>> (31 - 8)), 0, aTemp.size() - 1, 0, 8, (element, d, bits) -> {
+            Integer[] a = MSD.sort(aTemp.toArray(new Integer[0]), new Integer[aTemp.size()], (1024), 0, aTemp.size() - 1, 0, 10, (element, d, bits) -> {
                 int bitsTo = 32 - (d * bits);
                 if (bitsTo < 0) {
                     return -1;
@@ -54,36 +55,99 @@ public class Main {
                 aTemp.add(line);
             }
 
-            String[] a = MSD.sort(aTemp.toArray(new String[0]), new String[aTemp.size()], (-1 >>> (32 - 8)), 0, aTemp.size() - 1, 0, 8, (element, d, bits) -> {
-                byte[] bytes = element.getBytes();
-                int bitsFrom = d * bits;
-                if (bitsFrom >= bytes.length * 8) {
-                    return -1;
-                }
+            String[] a = aTemp.toArray(new String[0]);
+            String[] aux = new String[a.length];
+            int b = 16;
+            int R = (-1 >>> (32 - b));
+            int low = 0;
+            int high = a.length - 1;
 
-                int bitsTo = ((d + 1) * bits) - 1;
-                if (bitsTo > bytes.length * 8) {
-                    bitsTo = (bytes.length * 8) - 1;
-                }
-
-                int outputBits = 0;
-                for (int i = bitsFrom / 8; i <= bitsTo / 8; i++) {
-                    if (i == bitsFrom / 8) {
-                        if (i == bitsTo / 8) {
-                            return ((-1 >>> 32 - (bitsTo % 8)) & bytes[bitsTo / 8]) >>> (bitsFrom % 8);
+            String[] aSorted =
+                    MSD.sort(a, aux, R, low, high, 0, b, (element, d, bits) -> {
+                        int bitsFrom = d * bits;
+                        if (bitsFrom >= element.length() * 8) {
+                            return -1;
                         }
-                        outputBits = bytes[bitsFrom / 8] >>> ((bitsFrom % 8));
-                    } else if (i == bitsTo / 8) {
-                        outputBits = (outputBits << ((bitsTo % 8))) | (bytes[bitsTo / 8] >>> (7 - (bitsTo % 8)));
-                    } else {
-                        outputBits = (outputBits << 8) | bytes[i];
-                    }
-                }
 
-                return outputBits;
-            });
+                        int bitsTo = ((d + 1) * bits) - 1;
+                        if (bitsTo > element.length() * 8) {
+                            bitsTo = (element.length() * 8) - 1;
+                        }
 
-            for (String s : a) {
+                        int outputBits = 0;
+                        for (int i = bitsFrom / 8; i <= bitsTo / 8; i++) {
+                            if (i == bitsFrom / 8) {
+                                if (i == bitsTo / 8) {
+                                    return ((-1 >>> 32 - (bitsTo % 8)) & element.charAt(bitsTo / 8)) >>> (bitsFrom % 8);
+                                }
+                                outputBits = element.charAt(bitsFrom / 8) >>> ((bitsFrom % 8));
+                            } else if (i == bitsTo / 8) {
+                                outputBits = (outputBits << ((bitsTo % 8))) | (element.charAt(bitsTo / 8) >>> (7 - (bitsTo % 8)));
+                            } else {
+                                outputBits = (outputBits << 8) | element.charAt(i);
+                            }
+                        }
+
+                        return outputBits;
+                    });
+
+            for (String s : aSorted) {
+                bufferedWriter.write(s + "\n");
+            }
+            bufferedWriter.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sortTestStringOld(File file) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("Radix Sort/testOutput.txt"))) {
+            ArrayList<String> aTemp = new ArrayList<>();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                aTemp.add(line);
+            }
+
+            String[] a = aTemp.toArray(new String[0]);
+            String[] aux = new String[a.length];
+            int b = 16;
+            int R = (-1 >>> (32 - b));
+            int low = 0;
+            int high = a.length - 1;
+
+            String[] aSorted =
+                    MSD.sort(a, aux, R, low, high, 0, b, (element, d, bits) -> {
+                        byte[] bytes = element.getBytes();
+                        int bitsFrom = d * bits;
+
+                        if (bitsFrom >= bytes.length * 8) {
+                            return -1;
+                        }
+
+                        int bitsTo = ((d + 1) * bits) - 1;
+                        if (bitsTo > bytes.length * 8) {
+                            bitsTo = (bytes.length * 8) - 1;
+                        }
+
+                        int outputBits = 0;
+                        for (int i = bitsFrom / 8; i <= bitsTo / 8; i++) {
+                            if (i == bitsFrom / 8) {
+                                if (i == bitsTo / 8) {
+                                    return ((-1 >>> 32 - (bitsTo % 8)) & bytes[bitsTo / 8]) >>> (bitsFrom % 8);
+                                }
+                                outputBits = bytes[bitsFrom / 8] >>> ((bitsFrom % 8));
+                            } else if (i == bitsTo / 8) {
+                                outputBits = (outputBits << ((bitsTo % 8))) | (bytes[bitsTo / 8] >>> (7 - (bitsTo % 8)));
+                            } else {
+                                outputBits = (outputBits << 8) | bytes[i];
+                            }
+                        }
+
+                        return outputBits;
+                    });
+
+            for (String s : aSorted) {
                 bufferedWriter.write(s + "\n");
             }
             bufferedWriter.flush();
---
+++ Word Frequencies V2/src/Trie.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Trie {
+    private Node root;
+    private final HashMap<ParentChildConnection, Node> childMap = new HashMap<>();
+
+    private Node getChild(Node parent, char childLabelFirstChar) {
+        return childMap.get(new ParentChildConnection(parent, childLabelFirstChar));
+    }
+
+    public int get(String key) {
+        Node node = get(root, key, 0);
+        if (node == null) return -1;
+        return node.value;
+    }
+
+    private Node get(Node node, String key, int d) {
+        if (node == null) return null;
+        if (!key.startsWith(node.label, d)) return null;
+        if (key.length() == d + node.label.length()) return node;
+
+        return get(getChild(node, key.charAt(d + node.label.length())), key, d + node.label.length());
+    }
+
+    public void put(){
+
+    }
+
+    private static class Node {
+        private String label;
+
+        private int value;
+
+        public Node(String label) {
+            this.label = label;
+            this.value = -1;
+        }
+
+        public Node(String label, int value) {
+            this.label = label;
+            this.value = value;
+        }
+    }
+
+    private record ParentChildConnection(Node parent, char childLabelFirstChar) {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ParentChildConnection that = (ParentChildConnection) o;
+            return parent.equals(that.parent) && childLabelFirstChar == that.childLabelFirstChar;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(parent, childLabelFirstChar);
+        }
+    }
+}
