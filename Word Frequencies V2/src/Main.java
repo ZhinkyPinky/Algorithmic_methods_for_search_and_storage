c1adc07e053228bcfd634d2a0b36712105f4a97c
ZhinkyPinky <jonathan.engstrom@gmail.com>
2023-02-04 18:11:01 +0100

    Word Frequencies V2 start

--- Word Frequencies V2/src/Main.java
+++ Word Frequencies V2/src/Main.java
@@ -1,4 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        try (Scanner scanner = new Scanner((new BufferedReader(new FileReader("Word Frequencies V2/test.txt"))))) {
+            scanner.useDelimiter("\\W+");
+
+            Trie trie = new Trie();
+            while (scanner.hasNext()) {
+                String s = scanner.next().toLowerCase();
+
+                int value = trie.get(s);
+                if (value == -1) {
+                    trie.put(s, 1);
+                } else {
+                    trie.put(s, value + 1);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- Word Frequencies V2/src/Trie.java
+++ Word Frequencies V2/src/Trie.java
@@ -5,6 +5,10 @@ public class Trie {
     private Node root;
     private final HashMap<ParentChildConnection, Node> childMap = new HashMap<>();
 
+    public Trie() {
+        root = new Node(null);
+    }
+
     public int get(String key) {
         Node node = get(root, key, 0);
         if (node == null) return -1;
@@ -13,6 +17,7 @@ public class Trie {
 
     private Node get(Node node, String key, int d) {
         if (node == null) return null;
+        if (node.label == null) return null;
         if (!key.startsWith(node.label, d)) return null;
         if (key.length() == d + node.label.length()) return node;
 
@@ -23,22 +28,52 @@ public class Trie {
         return childMap.get(new ParentChildConnection(parent, childLabelFirstChar));
     }
 
-    public void put(String key) {
-        Node node = root;
-        int i = 0;
+    public void put(String key, int value) {
+        Node parentNode = getChild(root, key.charAt(0));
+        int keyPos = 0;
+        int depth = 0;
+
         while (true) {
-            node = getChild(node, key.charAt(i++));
+            if (parentNode != root && key.charAt(keyPos) == parentNode.label.charAt(keyPos - depth)) { //Parent label match key.
+                if (keyPos == (depth + parentNode.label.length() - 1)) { //End of parent-node label reached.
+                    depth += parentNode.label.length();
 
-            if (i < key.length() && node == null) {
+                    if (keyPos == (key.length() - 1)) { //Update value if key already exists.
+                        parentNode.value = value;
+                        return;
+                    }
 
+                    Node childNode = getChild(parentNode, key.charAt(keyPos));
+                    if (childNode == null) { //Create new node with value if key doesn't already exist.
+                        childMap.put(new ParentChildConnection(parentNode, key.charAt(keyPos)), new Node(key.substring(keyPos), value));
+                        return;
+                    } else { //Move to new parent-node if child with continuation of key exists.
+                        parentNode = childNode;
+                    }
+                }
+            } else { //Parent label doesn't match key.
+                if (parentNode != root) {
+                    childMap.put( //New node to split the parent node.
+                            new ParentChildConnection(parentNode, parentNode.label.charAt(keyPos - depth)),
+                            new Node(parentNode.label.substring(keyPos - depth)));
+
+                    //Update the parentNode.
+                    parentNode.label = parentNode.label.substring(0, keyPos - depth);
+                    parentNode.value = -1;
+                }
+
+                childMap.put( //New node to add the new key.
+                        new ParentChildConnection(parentNode, key.charAt(keyPos)),
+                        new Node(key.substring(keyPos), value));
             }
+
+            keyPos++;
         }
 
     }
 
     private static class Node {
         private String label;
-
         private int value;
 
         public Node(String label) {
